2f916a35ec997bdc316a5a9ea36dcc23669151fb
Steven Zhou <stevenz9@uci.edu>
2022-12-07 18:50:36 -0800

    day6

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,14 +1,14 @@
 # part1
-# unique = []
-# with open("input.txt", "r") as f:
-#     for i in f:
-#         for letter in i:
-#             unique.append(letter)
-#             if len(unique) > 4:
-#                 temp = set(unique[-4:])
-#                 if len(temp) == 4:
-#                     print(len(unique))
-#                     break
+unique = []
+with open("input.txt", "r") as f:
+    for i in f:
+        for letter in i:
+            unique.append(letter)
+            if len(unique) > 4:
+                temp = set(unique[-4:])
+                if len(temp) == 4:
+                    print(len(unique))
+                    break
 
 # part2
 unique = []
